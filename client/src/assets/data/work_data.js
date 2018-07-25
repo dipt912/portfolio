029848a1abca87e9d3343d91fdc235b6114c643d
Diptesh Patel <dipteshpatel@Dipteshs-MacBook-Pro.local>
2018-07-25 01:11:46 -0700

    update work item

--- client/src/assets/data/work_data.js
+++ client/src/assets/data/work_data.js
@@ -1,5 +1,6 @@
 import portfolio from '../images/work/portfolio.png';
 import contactBook from '../images/work/contactBook.png';
+import noteBook from '../images/work/noteBook.jpg';
 
 export default [
     
@@ -10,6 +11,14 @@ export default [
         alt: 'Portfolio',
         codeLink: "https://github.com/dipt912/portfolio",
         demoLink: "https://dipt912.github.io/portfolio/#"
+    },
+    {
+        name: 'NoteBook',
+        info: 'NoteBook using React',
+        img: noteBook,
+        alt: 'NoteBook',
+        codeLink: "https://github.com/dipt912/noteBook",
+        demoLink: "https://dipt912.github.io/noteBook"
     },
     {
         name: 'Contact-Manager',
